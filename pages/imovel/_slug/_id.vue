2e59afc636b559896f19f4d80772979b96ecd173
Renato Nakamura <65171479+Renato-Nakamura@users.noreply.github.com>
2023-09-15 13:54:01 -0300

    fix. image

--- pages/imovel/_slug/_id.vue
+++ pages/imovel/_slug/_id.vue
@@ -25,7 +25,7 @@ export default {
           {
             hid: "og:image",
             property: "og:image",
-            content: this.item.species,
+            content: this.item.image,
           },
           {
             hid: "og:url",
